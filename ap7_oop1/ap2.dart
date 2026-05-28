c4904006f899ef1a9195d12a6ab71ae119d11f17
joao-boing <joaovtboing@gmail.com>
2026-05-28 15:53:26 -0300

    Ativdade Ap7 Orientação Objetos: ap2 Pronta

---
+++ ap7_oop1/ap2.dart
@@ -0,0 +1,30 @@
+void main()
+  {
+    //Declarando o objeto Retangulo
+    var retangulo = Retangulo(14.52, 21.91);
+
+    //Declarando a área chamando o método calcularArea
+    var area = retangulo.calcularArea();
+
+    //Imprimindo a área
+    print("Área do Retângulo: $area");
+  }
+
+//Classe que representa um retângulo
+class Retangulo
+  {
+    //Declarando as variáveis
+    final double altura;
+    final double largura;
+    late double area;
+
+    //Construtor
+    Retangulo(this.altura, this.largura);
+
+    //Método para calcular área
+    double calcularArea()
+      {
+        double area = largura * altura;
+        return area;
+      }
+  }
