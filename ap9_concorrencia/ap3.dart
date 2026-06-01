c202aae5aaced664d8d4307735cbc3e2fb1414f5
joao-boing <joaovtboing@gmail.com>
2026-06-01 16:45:36 -0300

    Atividade Ap9 Concorrência: ap3 corrigida (Comentários e espera)

--- ap9_concorrencia/ap3.dart
+++ ap9_concorrencia/ap3.dart
@@ -1,17 +1,23 @@
 void main() async {
   print('Iniciando lançamento');
 
+  //Chama a função iniciar contagem(Só continua quando ela acabar)
   await iniciarContagem(10);
 
   print('Foguete lançado!');
 }
 
+//Função iniciarContagem
 Future<void> iniciarContagem(int cont) async
   {
+    //Loop para contagem
     while(cont > 0)
       {
+        //Espera 1 segundo para imprimir
         await Future.delayed(Duration(seconds: 1));
         print("$cont...");
         cont--;
       }
+    //Espera 1 segundo para acabar
+    await Future.delayed(Duration(seconds: 1));
   }
