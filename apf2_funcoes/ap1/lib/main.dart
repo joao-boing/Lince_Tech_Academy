53e1890425e06999f7dc4a8fe07c47460ea670a8
joao-boing <joaovtboing@gmail.com>
2026-06-15 15:15:23 -0300

    Atividade Flutter Apf2: ap1 Pronta

---
+++ apf2_funcoes/ap1/lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main()
+  {
+    runApp(MyApp());
+  }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp
+      (
+        theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: MyWidget()
+          )
+        )
+      );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.red
+              ),
+
+              SizedBox(
+                width: 16,
+                height: 16,
+              ),
+
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.green
+              ),
+
+              SizedBox(
+                width: 16,
+                height: 16,
+              ),
+
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.blue
+              ),
+            ],
+          ),
+
+          SizedBox(
+            height: 8,
+            width: 8,
+          ),
+
+          Stack(
+            children: [
+              Container(
+                width: 196,
+                height: 124,
+                color: Colors.yellow
+              ),
+
+              Positioned(
+                top: 8,
+                bottom: 8,
+                left: 8,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 100,
+                      color: Colors.purple
+                    ),
+
+                     SizedBox(
+                      width: 16,
+                    ),
+
+                    Container(
+                      width: 50,
+                      height: 100,
+                      color: Colors.cyan
+                    ),
+
+                    SizedBox(
+                      width: 16
+                    ),
+
+                    Positioned(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          color: Colors.purple
+                          ),
+
+                        SizedBox(
+                          height: 8,
+                        ),
+
+                        Container(
+                          width: 50,
+                          height: 50,
+                          color: Colors.cyan
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),      
+            ]
+          ),
+
+          SizedBox(
+            width: 8,
+            height: 8,
+          ),
+
+          Stack(
+            children: [
+              Container(
+                width: 100,
+                height: 66,
+                color: Colors.grey
+              ),
+
+              Positioned(
+                top: 8,
+                bottom: 8,
+                left: 25,
+                right: 25,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.black
+                )
+              )
+            ],
+          )
+
+        ],
+      )
+    );
+  }
+}
